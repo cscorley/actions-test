5b6539dec539ba309c80907bb399853d83f6b56a
Christopher S. Corley <chris.corley@fathom.video>
2022-06-08 13:22:11 -0400

    more json

--- test.sh
+++ test.sh
@@ -7,5 +7,12 @@ function set_output() {
     echo "::set-output name=$1::$2"
 }
 
+function set_output_json() {
+    echo "set-output name=$1::${{ toJSON($2) }}"
+    echo "::set-output name=$1::${{ toJSON($2) }}"
+}
+
 set_output "bool_true" "true"
 set_output "bool_false" "false"
+set_output_json "json_true" "true"
+set_output_json "json_false" "false"
